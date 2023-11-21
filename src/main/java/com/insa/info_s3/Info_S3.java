5823ce0687f9dcdcf6372f59aa04f6c4d82a353b
athouvenot01 <amandine.thouvenot@insa-strasbourg>
2023-11-21 14:09:34 +0100

    ça fonctionne ?

---
+++ src/main/java/com/insa/info_s3/Info_S3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.insa.info_s3;
+
+/**
+ *
+ * @author Amandine Tvt
+ */
+public class Info_S3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("ça fonctionne ouuuuuu");
+    }
+}
