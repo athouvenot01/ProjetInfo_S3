bca13c45dabf8e9664f5ac986f143f8cf36d5d8d
athouvenot01 <amandine.thouvenot@insa-strasbourg>
2023-11-21 15:49:31 +0100

    gestion bdd

--- src/main/java/com/insa/info_s3/GestionBDD.java
+++ src/main/java/com/insa/info_s3/GestionBDD.java
@@ -4,10 +4,50 @@
  */
 package com.insa.info_s3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Amandine Tvt
  */
 public class GestionBDD {
+    
+    
+    public static Connection connectGeneralMySQL(String host,
+            int port, String database,
+            String user, String pass)
+            throws SQLException {
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://" + host + ":" + port
+                + "/" + database,
+                user, pass);
+        con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+        return con;
+    }
+    
+    
+    public static Connection connectSurServeurM3() throws SQLException {
+        return connectGeneralMySQL("92.222.25.165", 3306,
+                "m3_lrosian01", "m3_lrosian01",
+                "30ea71e6");
+    }
+        
+        
+    public static void debut() {
+        try (Connection con = connectSurServeurM3()) {
+            System.out.println("connecté");
+        } catch (SQLException ex) {
+            throw new Error("Connection impossible", ex);
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        debut();
+    }
+    
+    
     
 }
