0ba3e4944cb9369d56dd798ed84857b3f0f77044
Emilien <Emilien@172.17.106.164>
2023-12-15 10:30:04 +0100

    interface console 2

--- src/main/java/com/insa/info_s3/Interface_Console.java
+++ src/main/java/com/insa/info_s3/Interface_Console.java
@@ -4,15 +4,52 @@
  */
 package com.insa.info_s3;
 
+import static com.insa.info_s3.GestionBDD.*;
+
 /**
  *
  * @author Emilien
  */
 public class Interface_Console {
-
+    
+public static void main(String[] args) {
+        InterfaceC();  
+    }
+    
     
 public static void InterfaceC (){
+    //debut();
+    int i;
+    String choix;
     System.out.println("Quelle table voulez vous modifier ?(1=produit,2=machine,3=opération,4=renitialisation");
-
+    i=Lire.i();
+    switch (i){
+    case 1 -> { 
+        System.out.println("Créer ou supprimer produit ?");
+        choix=Lire.S();
+        if (choix=="Créer") {
+        
+        }
+        else if (choix=="supprimer") {
+        
+        }
+        break;
+        }
+    case 2 -> { 
+       System.out.println("Créer ou supprimer machine ?");
+       choix=Lire.S();
+        break;
+        }
+    case 3 -> { 
+        System.out.println("Créer ou supprimer oppération ?");
+        choix=Lire.S();
+        break;
+        }
+    case 4 -> { 
+        System.out.println("réenitialiser?");
+        choix=Lire.S();
+        break;
+        }
+    }
 }
 }
