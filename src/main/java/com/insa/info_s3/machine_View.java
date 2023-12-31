b0030d1ca607afff73fde124d492480897b337bb
loicrosian <loicrosian@localhost>
2023-12-31 11:01:39 +0100

    ajout

---
+++ src/main/java/com/insa/info_s3/machine_View.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.insa.info_s3;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+/**
+ *
+ * @author loicrosian
+ */
+
+@Route(value = "machine_View", layout = UI.class)
+public class machine_View extends Div {
+    
+}
