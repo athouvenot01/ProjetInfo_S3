06f5248f8bdf6f73fbdbb79658a1879859f9aeb7
amand <amand@ASUS_28-11>
2024-01-08 16:15:08 +0100

    état machine + opérateur fini

--- src/main/java/com/insa/info_s3/Interface_Console.java
+++ src/main/java/com/insa/info_s3/Interface_Console.java
@@ -16,16 +16,22 @@ import java.util.logging.Logger;
  */
 
 public class Interface_Console {
-    /*
+    
+    
     public static void main(String[] args) {
-        InterfaceC();  
+        try (Connection con = GestionBDD.connectSurServeurM3()) {
+            InterfaceC(con);  
+            
+        } catch (SQLException ex) {
+            System.out.println("probleme : " + ex.getLocalizedMessage());
+            ex.printStackTrace();
+        }
     }
     
     
-    public static void InterfaceC (){
+    public static void InterfaceC (Connection con){
         String ref,des;
-        int idmateriaux;
-        try (Connection con = GestionBDD.connectSurServeurM3()) {
+        int idmateriaux, puissance, id, etatmachine;
             int i;
             int choix;
             System.out.println("Quelle table voulez vous modifier ?(1=produit,2=machine,3=operation,4=renitialisation");
@@ -39,18 +45,53 @@ public class Interface_Console {
                         ref=Lire.S();
                         System.out.println("Pourrais je avoir votre description ?");
                         des=Lire.S();
-                        System.out.println("Pourrais je avoir votre description ?");
+                        System.out.println("Pourrais je avoir votre materiaux ?");
                         idmateriaux=Lire.i();
-                        GestionBDD.createProduit(con, ref, des, idmateriaux);
+                        try {
+                            GestionBDD.createProduit(con, ref, des, idmateriaux);
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Interface_Console.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
                     else if(choix==2) {
-     
+                        System.out.println("Quel produit voulez vous supprimer ?");
+                        id=Lire.i();
+                        try {
+                            GestionBDD.deleteProduit(con, id);
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Interface_Console.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
                     break;
                 }
+                
                 case 2 -> {
                     System.out.println("Créer ou supprimer machine ?");
                     choix=Lire.i();
+                    if (choix==1) {
+                        System.out.println("Pourrais je avoir votre reference ?");
+                        ref=Lire.S();
+                        System.out.println("Pourrais je avoir votre description ?");
+                        des=Lire.S();
+                        System.out.println("Pourrais je avoir votre puissance ?");
+                        puissance=Lire.i();
+                        System.out.println("Pourrais je avoir votre état ?");
+                        etatmachine=Lire.i();
+                        try {
+                            GestionBDD.createMachine(con, ref, des, puissance, etatmachine);
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Interface_Console.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    else if(choix==2) {
+                        System.out.println("Quel machine voulez vous supprimer ?");
+                        id=Lire.i();
+                        try {
+                            GestionBDD.deleteMachine(con, id);
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Interface_Console.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
                     break;
                 }
                 case 3 -> {
@@ -64,9 +105,6 @@ public class Interface_Console {
                     break;
                 }
             }
-        } catch (SQLException ex) {
-            System.out.println("probleme : " + ex.getLocalizedMessage());
-            ex.printStackTrace();
-        }
-    }*/
+        
+    }
 }
