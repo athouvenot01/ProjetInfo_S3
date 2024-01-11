3990a8e92860fc301b00291b07614d41d41570db
Emilien <Emilien@PCdeMICHMICH>
2024-01-11 23:02:37 +0100

    Machines

---
+++ src/main/java/com/insa/info_s3/Machines.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.insa.info_s3;
+
+/**
+ *
+ * @author Emilien
+ */
+public class Machines {
+    public static class Machine {
+        private String ref;
+        private String des;
+        private int puissance;
+        private int id;
+        private int etatmachine;
+
+        public Machine(int id, String ref, String des, int puissance , int etatmachine) {
+            this.id = id;
+            this.ref = ref;
+            this.des = des;
+            this.puissance = puissance;
+            this.etatmachine =etatmachine;
+        }
+
+        public String getRef() {
+            return ref;
+        }
+
+        public void setRef(String ref) {
+            this.ref = ref;
+        }
+
+        public String getDes() {
+            return des;
+        }
+
+        public void setDes(String des) {
+            this.des = des;
+        }
+
+        public int getPuissance() {
+            return puissance;
+        }
+
+        public void setPuissance(int puissance) {
+            this.puissance = puissance;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getEtatmachine() {
+            return etatmachine;
+        }
+
+        public void setEtatmachine(int etatmachine) {
+            this.etatmachine = etatmachine;
+        }
+        
+    }
+}
